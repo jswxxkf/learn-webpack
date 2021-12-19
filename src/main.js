5a654907dc63f6d637ebad81020f574a44fbce03
jswxxkf <xuekaifeng012@gmail.com>
2021-12-19 22:38:57 +0800

    feat: 基础模块及插件的配置

--- src/main.js
+++ src/main.js
@@ -1,5 +1,32 @@
 import { printInfo } from "./js/foo.js";
 const { address } = require("./js/bar.js");
 
+import "./css/title.css";
+import "./css/image.less";
+import "./font/iconfont.css";
+
 printInfo();
 console.log(address);
+
+const titleEl = document.createElement("div");
+titleEl.innerHTML = "Hello Webpack!";
+titleEl.className = "title";
+document.body.appendChild(titleEl);
+
+const subTitleEl = document.createElement("div");
+subTitleEl.innerHTML = "你好啊，Webpack！";
+subTitleEl.className = "subtitle";
+document.body.appendChild(subTitleEl);
+
+const imgEl = document.createElement("img");
+imgEl.src = require("./img/2.jpg");
+imgEl.className = "image";
+document.body.appendChild(imgEl);
+
+const bgEl = document.createElement("div");
+bgEl.className = "bg";
+document.body.appendChild(bgEl);
+
+const iEl = document.createElement("i");
+iEl.className = "iconfont icon-aixin";
+document.body.appendChild(iEl);
--- wk.config.js
+++ wk.config.js
@@ -1,9 +1,83 @@
 const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { DefinePlugin } = require("webpack");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/main.js",
   output: {
-    filename: "bundle.js",
+    filename: "js/bundle.js",
     path: path.resolve(__dirname, "./build"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2,
+            },
+          },
+          "postcss-loader",
+          "less-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|svg|gif)$/,
+        type: "asset",
+        generator: {
+          filename: "img/[name].[hash:6][ext]",
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 100 * 1024,
+          },
+        },
+      },
+      {
+        test: /\.(ttf|eot|woff2?|json)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "font/[name].[hash:6][ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      title: "KaifengXue Webpack",
+      template: "./public/index.html",
+    }),
+    new DefinePlugin({
+      BASE_URL: "'./'",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "public",
+          globOptions: {
+            ignore: ["**/index.html", "**/.DS_Store"],
+          },
+        },
+      ],
+    }),
+  ],
 };
